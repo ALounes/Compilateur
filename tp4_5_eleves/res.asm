45852049ce07678db5b8c4fc04262f3bbf9598ec
MaximeMartelli <maxime.mmartelli@gmail.com>
2015-02-27 11:32:26 +0100

    If et while vérifiés

--- tp4_5_eleves/res.asm
+++ tp4_5_eleves/res.asm
@@ -1,23 +1,31 @@
 main:
 	push 0
 	move 1
-	push 1
-	move 1
-	push 2
-	move 1
+	push	0
+	copy 1	
+	pushr	1.000000
+	ls
+	jf Else_if_0
 	push	0
 	copy 1	
 	output
-	push	1
-	copy 1	
+	jp Next_if_0
+Else_if_0
+	pushr	3.000000
 	output
-	push	2
+Next_if_0:
+L_while_1:
+	push	0
 	copy 1	
-	output
+	pushr	2.000000
+	ls
+	jf Next_while_1
 	push 0
 	move 1
 	push	0
 	copy 1	
 	output
+	jp L_while_1
+Next_while_1:
 	halt
 	end
